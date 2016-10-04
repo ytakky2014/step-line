a1c70a12480f9b9794183290fdfbe7551fa97157
aa-ta_yoshida <ta_yoshida@corp.allabout.co.jp>
2016-10-04 20:02:33 +0900

    fix variable

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # check slack access token
-if [ -z "$WERCKER_LINE_NOTIFIER_PERSONAL_ACCESS_TOKEN" ]; then
+if [ -z "$WERCKER_TOKEN" ]; then
     fail "Please provide a LINE NOTIFY PERSONAL ACCESS TOKEN"
 fi
 
 export MESSAGE="$WERCKER_APPLICATION_NAME : $WERCKER_RESULT "
 
 
-curl -v -X POST -H "Authorization: Bearer $WERCKER_LINE_NOTIFIER_PERSONAL_ACCESS_TOKEN" -F "message=$MESSAGE" https://notify-api.line.me/api/notify
+curl -v -X POST -H "Authorization: Bearer $WERCKER_TOKEN" -F "message=$MESSAGE" https://notify-api.line.me/api/notify
